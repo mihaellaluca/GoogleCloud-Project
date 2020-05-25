0a2ba2daa00d1619d70624fe7a6e5191a20ee1d8
AmarandeiGeorge <gamarandei20@yahoo.com>
2020-05-25 22:23:25 +0300

    test

--- FoodTalk-FE/src/app/app-routing.module.ts
+++ FoodTalk-FE/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookingComponent } from './components/booking/booking.component';
-
+import { MapComponent } from './components/map/map.component';
 
 const routes: Routes = [
-  { path: 'booking-component', component: BookingComponent }
-
+	{ path: 'booking-component', component: BookingComponent },
+	{ path: 'map-component', component: MapComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- FoodTalk-FE/src/app/app.module.ts
+++ FoodTalk-FE/src/app/app.module.ts
@@ -2,25 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BookingComponent } from './components/booking/booking.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    BookingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
-    
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HeaderComponent, BookingComponent, MapComponent ],
+	imports: [ BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, GoogleMapsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
