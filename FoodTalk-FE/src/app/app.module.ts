4bef9d9fff0894fb48b0bee359de2e8686076d23
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2020-05-26 16:58:49 +0300

    auth

--- FoodTalk-FE/src/app/app.module.ts
+++ FoodTalk-FE/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BookingComponent } from './components/booking/booking.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AuthService } from './services/auth/auth.service'
+// ...omitted
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {apiKey: "AIzaSyD4Gupl0obJnBa5K9HHDlhAglKkH2W25sw",
+  authDomain: "astral-bit-278316.firebaseapp.com",
+  databaseURL: "https://astral-bit-278316.firebaseio.com/",
+  projectId: "astral-bit-278316",
+  storageBucket: "astral-bit-278316.appspot.com",
+  messagingSenderId: "655163274876",
+  appId: "1:655163274876:web:f0558d95983c919225fbf9",
+  measurementId: "G-XERHT73D69"}
+
+
+
 
 @NgModule({
   declarations: [
@@ -17,10 +37,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
-    
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
