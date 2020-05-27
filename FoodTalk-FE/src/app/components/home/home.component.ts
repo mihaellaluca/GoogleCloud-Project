f13db26526ab55e22908558bb220e34ba36e13ed
AmarandeiGeorge <gamarandei20@yahoo.com>
2020-05-27 04:35:39 +0300

    Update home.component.ts

--- FoodTalk-FE/src/app/components/home/home.component.ts
+++ FoodTalk-FE/src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from './../../services/auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: [ './home.component.scss' ]
 })
 export class HomeComponent implements OnInit {
-	constructor() {}
+	constructor(private http: HttpClient, private authService: AuthService) {}
 	users = [
 		{
 			displayName: 'Robert Tataru',
@@ -19,6 +20,24 @@ export class HomeComponent implements OnInit {
 		}
 	];
 	ngOnInit(): void {
-		console.log(this.users[0]);
+		this.authService.user$.pipe().subscribe((data) => {
+			this.http
+				.get('https://us-central1-astral-bit-278316.cloudfunctions.net/getUserbyEmail/' + data['email'])
+				.pipe()
+				.subscribe((data) => {
+					console.log(data);
+					let userProfile = Object.keys(data).map((i) => data[i]);
+					let uid = userProfile[0].uid;
+					this.http
+						.get(
+							`
+          https://us-central1-astral-bit-278316.cloudfunctions.net/getNearestUsers/${uid}`
+						)
+						.pipe()
+						.subscribe((data) => {
+							this.users = Object.keys(data).map((i) => data[i]);
+						});
+				});
+		});
 	}
 }
