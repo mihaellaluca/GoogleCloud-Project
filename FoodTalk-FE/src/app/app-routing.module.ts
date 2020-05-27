59aea157940556e31ceb9ea024f950b8f6ba45ec
mihaellaluca <48389197+mihaellaluca@users.noreply.github.com>
2020-05-27 04:55:19 +0300

    plus

--- FoodTalk-FE/src/app/app-routing.module.ts
+++ FoodTalk-FE/src/app/app-routing.module.ts
@@ -1,25 +1,44 @@
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { AuthGuard } from './guards/auth.guard';
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { BookingComponent } from './components/booking/booking.component';
-import { MapComponent } from './components/map/map.component';
+import { UserProfileComponent } from "./components/user-profile/user-profile.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BookingComponent } from "./components/booking/booking.component";
+import { MapComponent } from "./components/map/map.component";
 
-import { HomeComponent } from './components/home/home.component';
-import { ChatComponent} from './components/chat/chat.component';
-
+import { HomeComponent } from "./components/home/home.component";
+import { ChatComponent } from "./components/chat/chat.component";
 
 const routes: Routes = [
-
-	{ path: 'booking-component', component: BookingComponent,canActivate: [AuthGuard] },
-  { path: 'map-component', component: MapComponent,canActivate: [AuthGuard] },
-  {path: 'user-profile-component', component: UserProfileComponent, canActivate: [AuthGuard]},
-	{ path: 'home-component', component: HomeComponent, canActivate: [ AuthGuard ] },
-	{ path: 'chat-component', component: ChatComponent, canActivate: [ AuthGuard ] }
+  {
+    path: "booking-component",
+    component: BookingComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: "map-component", component: MapComponent, canActivate: [AuthGuard] },
+  {
+    path: "user-profile-component",
+    component: UserProfileComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: "home-component",
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: "chat-component",
+    component: ChatComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: "",
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+  }
 ];
 
 @NgModule({
-	imports: [ RouterModule.forRoot(routes) ],
-	exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
