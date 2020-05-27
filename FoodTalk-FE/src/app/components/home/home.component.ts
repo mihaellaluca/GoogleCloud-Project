9b795c03dee95c9e1d26abe29a57bca528ee134c
mihaellaluca <48389197+mihaellaluca@users.noreply.github.com>
2020-05-27 04:43:57 +0300

    chicken style 3

--- FoodTalk-FE/src/app/components/home/home.component.ts
+++ FoodTalk-FE/src/app/components/home/home.component.ts
@@ -1,43 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './../../services/auth/auth.service';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./../../services/auth/auth.service";
+import { HttpClient } from "@angular/common/http";
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.component.html',
-	styleUrls: [ './home.component.scss' ]
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-	constructor(private http: HttpClient, private authService: AuthService) {}
-	users = [
-		{
-			displayName: 'Robert Tataru',
-			email: 'robert@gmail.com',
-			photoUrl:
-				'https://lh6.googleusercontent.com/-FtfAqmIDu_8/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnwee9o5l03SRsgV5y8UkNmEmgCGg/photo.jpg',
-			prefFood: [ 'sea fruits', 'greek salad' ],
-			prefRestaurants: [ 'oscar' ],
-			prefSpecific: [ 'mediteranean' ]
-		}
-	];
-	ngOnInit(): void {
-		this.authService.user$.pipe().subscribe((data) => {
-			this.http
-				.get('https://us-central1-astral-bit-278316.cloudfunctions.net/getUserbyEmail/' + data['email'])
-				.pipe()
-				.subscribe((data) => {
-					console.log(data);
-					let userProfile = Object.keys(data).map((i) => data[i]);
-					let uid = userProfile[0].uid;
-					this.http
-						.get(
-							`
+  constructor(private http: HttpClient, private authService: AuthService) {}
+  users = [];
+  ngOnInit(): void {
+    this.authService.user$.pipe().subscribe((data) => {
+      this.http
+        .get(
+          "https://us-central1-astral-bit-278316.cloudfunctions.net/getUserbyEmail/" +
+            data["email"]
+        )
+        .pipe()
+        .subscribe((data) => {
+          console.log(data);
+          let userProfile = Object.keys(data).map((i) => data[i]);
+          let uid = userProfile[0].uid;
+          this.http
+            .get(
+              `
           https://us-central1-astral-bit-278316.cloudfunctions.net/getNearestUsers/${uid}`
-						)
-						.pipe()
-						.subscribe((data) => {
-							this.users = Object.keys(data).map((i) => data[i]);
-						});
-				});
-		});
-	}
+            )
+            .pipe()
+            .subscribe((data) => {
+              this.users = Object.keys(data).map((i) => data[i]);
+            });
+        });
+    });
+  }
 }
